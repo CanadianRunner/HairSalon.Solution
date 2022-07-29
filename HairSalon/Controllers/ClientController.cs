8d99abcc1bf08f6caa56c3c26d31479cb858b51e
Sean Keane <seankeane93@gmail.com>
2022-07-29 13:58:18 -0700

    WIP Client Controller logic and added home button to Stylists page

---
+++ HairSalon/Controllers/ClientController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace HairSalon.Controllers
+{
+  public class ClientsController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public ClientsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Client> data = _db.Clients.Include(client => client.Stylists).ToList();
+      return View(data);
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.Stylists = _db.Stylists.ToList();
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
